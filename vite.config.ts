00862f193edb8641a70750eed0040a18acd3d1d9
KeeeVW <kevinwael32@gmail.com>
2026-08-14 03:43:59 +0300

    Deploy site

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,1 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
-
-export default defineConfig({
-  base: "/troop-10-presentation/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+/// <reference types="vite/client" />
